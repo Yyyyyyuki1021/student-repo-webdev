9d21d1bf5c7bcee7406a993c95921e3ff36eadee
Yyyyyyuki1021 <yyamada@pdx.edu>
2024-02-02 06:27:03 +0900

    first commit

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Yuki Yamada.I'm senior. I like to play soccer and video games in my free time. This term I'm taking 4 classes, so I'm exhausted everyday. nice to meet you guys."
+  );
 }
